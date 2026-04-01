30c6e0bb5648009b27e71361f5a228e7a2c013fc
Heitor Cavalcanti <heitor.00000856227@unicap.br>
2026-04-01 12:58:03 -0300

    Exercicio 1

--- Jhey/Cassino/Roleta.java
+++
@@ -1,5 +0,0 @@
-package Cassino;
-
-public class Roleta{
-
-}
---
+++ Jhey/Exercícios Prova/Matriz.java
@@ -0,0 +1,30 @@
+package Jhey;
+import java.util.Scanner;
+
+public class Matriz {
+    private int[][] matriz = new int[3][3];
+
+    public void lerMatriz() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                System.out.print("matriz[" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+    }
+
+    public void imprimirTransposta() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++)
+                System.out.printf("%4d", matriz[j][i]);
+            System.out.println(); 
+        }
+    }
+
+    public static void main(String[] args) {
+        Matriz m = new Matriz();
+        m.lerMatriz();
+        System.out.println("Transposta:");
+        m.imprimirTransposta();
+    }
+}
